dd07f1efb0f465738d87778c612dc245c92f3f03
varun <varunp1001@gmail.com>
2025-06-12 23:51:44 +0530

    Updated frontend

--- frontend/src/components/contact.tsx
+++ frontend/src/components/contact.tsx
@@ -12,7 +12,7 @@ const Contact=()=>{
         e.preventDefault();
 
         try {
-            const response = await fetch("http://localhost:5000/api/contact", {
+            const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/contact`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
